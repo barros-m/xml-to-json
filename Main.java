d9bd8c88ee8466c8af66778d751ce04b4d865cf8
Matheus Barros <matheusbarros1305@gmail.com>
2021-07-15 22:20:01 -0400

    Program is functionally working

--- Main.java
+++ Main.java
@@ -6,6 +6,9 @@ import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
+import java.util.Properties;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 /*********************************************************** 
  *  Create java command line utility which is capable to:
@@ -22,36 +25,82 @@ import java.io.File;
 public class Main {
     public static void main(String [] args) {
 
+        Scanner in = new Scanner(System.in);
         try {
-            File file = new File("/Users/mattheusbarroos/Desktop/converter/AddressBook.xml");
+            ///Users/mattheusbarroos/Desktop/converter/Main.java
+            ///Users/mattheusbarroos/Desktop/converter/AddressBook.xml
+            System.out.println("Please enter full path for the xml file (including file name):");
+            
+            String filePath = in.nextLine();
+            
+            // Prepare document
+            File file = new File(filePath.toString());
             Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
             document.getDocumentElement().normalize();
+            
+            String result = "";
 
             NodeList contacts = document.getElementsByTagName("Contact");
+            result += "{\n\"AddressBook\": {\n\"Contact\": [\n"; 
             
             for(int i = 0, numberOfContacts = contacts.getLength(); i < numberOfContacts; i++) {
                 Node node = contacts.item(i);
                 if(node.getNodeType() == Node.ELEMENT_NODE) {
-                    System.out.println("{");
+                    //System.out.println("\t{");
+                    result += "\t{\n";
                     for(int j = 0, numberOfChildren = node.getChildNodes().getLength(); j< numberOfChildren; j++) {
                         Node element = node.getChildNodes().item(j);
                         if(element.getNodeType() == Node.ELEMENT_NODE) {
-                            System.out.print("\t\"" + element.getNodeName() + "\": \"" + element.getTextContent() + "\"");
+                            result += "\t\t\"" + element.getNodeName() + "\": \"" + element.getTextContent() + "\"";
                             if(j != numberOfChildren - 2) {
-                                System.out.println(", ");
+                                //System.out.println(", ");
+                                result += ",\n";
                             }
                             else {
-                                System.out.print("\n}");
+                                // System.out.print("\n\t}");
+                                result += "\n\t}";
                             }
                         }
                     }
                     if(i != numberOfContacts - 1) {
-                        System.out.println(", ");
+                        result += ",\n";
+                        // System.out.println(", ");
                     }
                 }
             }
-        } catch(Exception e) {
-            System.out.println("This was an error");
+            result += "\n\t]\n}\n}";
+
+            System.out.println("Do you want to (Please enter 1 or 2):\n1) save to a file or,\n2) display in the terminal?");
+            int r = 0;
+            do {
+                r = Integer.parseInt(in.nextLine());
+                if(r == 1) {
+                    try {
+                        String fileName = "results.json";
+                        System.out.println("Saving results to a file...\nDone. Please check " + fileName);
+                        FileWriter myWriter = new FileWriter(fileName);
+                        myWriter.write(result);
+                        myWriter.close();
+                    } catch(Exception e) {
+                        System.out.println("Please try a different way, or delete the file that you provided");
+                    }
+                }
+                else if (r == 2) {
+                    System.out.println("Showing results...");
+                    System.out.println(result);
+                }
+                else {
+                    System.out.println("Wrong option. Please try again. 1 or 2 only.");
+                }
+            } while(r != 1 && r != 2);
         }
+        
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("This was an error; Please try again with a different xml file path");
+            in.close();
+        }
+        in.close();
     }
+    
 }
